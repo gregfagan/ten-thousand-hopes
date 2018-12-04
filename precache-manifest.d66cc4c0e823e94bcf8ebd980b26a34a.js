4fadcb0e5454aa48b7ac7c6512f3bb8b8ab80e9c
Greg Fagan <gregfagan@gmail.com>
2018-12-03 20:38:47 -0600

    Updates

---
+++ precache-manifest.d66cc4c0e823e94bcf8ebd980b26a34a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e01d55871bb386c6910",
+    "url": "/ten-thousand-hopes/static/js/runtime~main.5e01d558.js"
+  },
+  {
+    "revision": "c2834faac9439e1daf21",
+    "url": "/ten-thousand-hopes/static/js/main.c2834faa.chunk.js"
+  },
+  {
+    "revision": "2b59ba01e24b9d57a99d",
+    "url": "/ten-thousand-hopes/static/js/1.2b59ba01.chunk.js"
+  },
+  {
+    "revision": "c2834faac9439e1daf21",
+    "url": "/ten-thousand-hopes/static/css/main.131de368.chunk.css"
+  },
+  {
+    "revision": "69b38963500859c6a7321bd64da9a44d",
+    "url": "/ten-thousand-hopes/index.html"
+  }
+];
